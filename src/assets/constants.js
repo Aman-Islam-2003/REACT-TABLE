c4027dccf289ce1252589ff18b42752186601990
Aman-Islam-2003 <amanislam019@gmail.com>
2024-05-18 13:36:36 +0500

    basic file structure

---
+++ src/assets/constants.js
@@ -0,0 +1,253 @@
+export const data = [
+    {
+      "id": 1,
+      "gender": "male",
+      "salary": "200"
+    },
+    {
+      "id": 2,
+      "gender": "female",
+      "salary": "100"
+    },
+    {
+      "id": 3,
+      "gender": "male",
+      "salary": "900"
+    },
+    {
+      "id": 4,
+      "gender": "female",
+      "salary": "500"
+    },
+    {
+      "id": 5,
+      "gender": "male",
+      "salary": "300"
+    },
+    {
+      "id": 6,
+      "gender": "female",
+      "salary": "600"
+    },
+    {
+      "id": 7,
+      "gender": "male",
+      "salary": "400"
+    },
+    {
+      "id": 8,
+      "gender": "female",
+      "salary": "700"
+    },
+    {
+      "id": 9,
+      "gender": "male",
+      "salary": "800"
+    },
+    {
+      "id": 10,
+      "gender": "female",
+      "salary": "200"
+    },
+    {
+      "id": 11,
+      "gender": "male",
+      "salary": "1000"
+    },
+    {
+      "id": 12,
+      "gender": "female",
+      "salary": "300"
+    },
+    {
+      "id": 13,
+      "gender": "male",
+      "salary": "400"
+    },
+    {
+      "id": 14,
+      "gender": "female",
+      "salary": "500"
+    },
+    {
+      "id": 15,
+      "gender": "male",
+      "salary": "600"
+    },
+    {
+      "id": 16,
+      "gender": "female",
+      "salary": "700"
+    },
+    {
+      "id": 17,
+      "gender": "male",
+      "salary": "800"
+    },
+    {
+      "id": 18,
+      "gender": "female",
+      "salary": "900"
+    },
+    {
+      "id": 19,
+      "gender": "male",
+      "salary": "200"
+    },
+    {
+      "id": 20,
+      "gender": "female",
+      "salary": "300"
+    },
+    {
+      "id": 21,
+      "gender": "male",
+      "salary": "400"
+    },
+    {
+      "id": 22,
+      "gender": "female",
+      "salary": "500"
+    },
+    {
+      "id": 23,
+      "gender": "male",
+      "salary": "600"
+    },
+    {
+      "id": 24,
+      "gender": "female",
+      "salary": "700"
+    },
+    {
+      "id": 25,
+      "gender": "male",
+      "salary": "800"
+    },
+    {
+      "id": 26,
+      "gender": "female",
+      "salary": "900"
+    },
+    {
+      "id": 27,
+      "gender": "male",
+      "salary": "100"
+    },
+    {
+      "id": 28,
+      "gender": "female",
+      "salary": "200"
+    },
+    {
+      "id": 29,
+      "gender": "male",
+      "salary": "300"
+    },
+    {
+      "id": 30,
+      "gender": "female",
+      "salary": "400"
+    },
+    {
+      "id": 31,
+      "gender": "male",
+      "salary": "500"
+    },
+    {
+      "id": 32,
+      "gender": "female",
+      "salary": "600"
+    },
+    {
+      "id": 33,
+      "gender": "male",
+      "salary": "700"
+    },
+    {
+      "id": 34,
+      "gender": "female",
+      "salary": "800"
+    },
+    {
+      "id": 35,
+      "gender": "male",
+      "salary": "900"
+    },
+    {
+      "id": 36,
+      "gender": "female",
+      "salary": "1000"
+    },
+    {
+      "id": 37,
+      "gender": "male",
+      "salary": "200"
+    },
+    {
+      "id": 38,
+      "gender": "female",
+      "salary": "300"
+    },
+    {
+      "id": 39,
+      "gender": "male",
+      "salary": "400"
+    },
+    {
+      "id": 40,
+      "gender": "female",
+      "salary": "500"
+    },
+    {
+      "id": 41,
+      "gender": "male",
+      "salary": "600"
+    },
+    {
+      "id": 42,
+      "gender": "female",
+      "salary": "700"
+    },
+    {
+      "id": 43,
+      "gender": "male",
+      "salary": "800"
+    },
+    {
+      "id": 44,
+      "gender": "female",
+      "salary": "900"
+    },
+    {
+      "id": 45,
+      "gender": "male",
+      "salary": "1000"
+    },
+    {
+      "id": 46,
+      "gender": "female",
+      "salary": "200"
+    },
+    {
+      "id": 47,
+      "gender": "male",
+      "salary": "300"
+    },
+    {
+      "id": 48,
+      "gender": "female",
+      "salary": "400"
+    },
+    {
+      "id": 49,
+      "gender": "male",
+      "salary": "500"
+    },
+    {
+      "id": 50,
+      "gender": "female",
+      "salary": "600"
+    }
+  ]
+  
